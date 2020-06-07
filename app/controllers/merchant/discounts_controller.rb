d4857609b14066b503a7b3497ec39f5f15079b48
Derek Borski <derek.borski@gmail.com>
2020-06-06 18:50:01 -0600

    Add link to discounts index and update index action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,6 +1,8 @@
 class Merchant::DiscountsController < Merchant::BaseController
 
   def index
+    @merchant = Merchant.find(current_user.merchant_id)
+    @discounts = @merchant.discounts.active_discounts
   end 
 
   def new
