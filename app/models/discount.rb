e0a43b02b8cbca905348f13f52869a8e7129c98d
Derek Borski <derek.borski@gmail.com>
2020-06-06 18:42:19 -0600

    Add active_discounts model method and spec

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,4 +2,8 @@ class Discount < ApplicationRecord
   belongs_to :merchant
 
   validates_presence_of :name, :percent_off, :quantity_required
+
+  def self.active_discounts
+    Discount.where(active: true)
+  end 
 end
--- spec/models/discount.rb
+++ spec/models/discount.rb
@@ -15,4 +15,26 @@ RSpec.describe Discount do
     before :each do
     end
   end
+
+  describe 'Class Methods' do
+    before :each do
+
+      @discount1 = create(:discount)
+      @discount2 = create(:discount)
+      @discount3 = create(:discount)
+      @discount4 = create(:discount)
+      @discount5 = create(:discount, active: false)
+      @discount6 = create(:discount, active: false)
+    end
+    it "active_discounts" do
+      
+
+      expect(Discount.active_discounts.include?(@discount1)).to eq(true)
+      expect(Discount.active_discounts.include?(@discount2)).to eq(true)
+      expect(Discount.active_discounts.include?(@discount3)).to eq(true)
+      expect(Discount.active_discounts.include?(@discount4)).to eq(true)
+      expect(Discount.active_discounts.include?(@discount5)).to eq(false)
+      expect(Discount.active_discounts.include?(@discount6)).to eq(false)
+    end
+  end
 end
